143aa03512d5129674cd8a9d985f65c4fc423314
praveenkumar.v <praveenkumar18113@cse.ssn.edu.in>
2022-08-01 14:24:43 +0530

    Method added

--- src/main/java/com/example/controller/DemoController.java
+++ src/main/java/com/example/controller/DemoController.java
@@ -1,10 +1,16 @@
 package com.example.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 
 
 public class DemoController {
-
+	@RequestMapping(value="/")
+	public String method1() {
+		return("Hi");
+	}
+	
+	
 }
